990509e8b47b5a13c40322409437c7c779e7270e
dani-sj <aliabuotorabi@gmail.com>
2016-02-18 21:33:39 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -243,14 +243,14 @@ function create_config( )
     "info",
     "lock_join"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {167424169},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 https://github.com/BH-YAGHI/NOD32-BOT.git
 
-channel : @Nod32team
-sodu : @behrooZyaghi
+channel : @dlovve
+sodu : @Xx_sarlashGar_oldwolf_xx
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -315,11 +315,11 @@ Only sudo users can run this command
 !bc 123456789 Hello !
 This command will send text to [group_id]
 
-ch: @Nod32team
+ch: @dlovve
 
 ]],
     help_text = [[
-NOD32 Commands list :
+help Commands list :
  
 1-banhammer list ^
  
@@ -487,10 +487,10 @@ NOD32 Commands list :
 you can use both "/" and "!" 
 .شما میتوانید از ! و / استفاده کنید
 
-Developer: @BehroozYaghi
+Developer: @Xx_sarlashGar_oldwolf_xx
 توسعه دهنده
 
-channel: @Nod32team
+channel: @dlovve
 کانال ما
 
 G00D LUCK ^_^
